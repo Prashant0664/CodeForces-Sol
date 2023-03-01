3c9a7c1634706b3c16227a226bf7377f04ad6a78
Thakran <prashantthakran0664@gmail.com>
2023-03-01 21:13:01 +0530

    28 FEB 2023 CHALLANGE

--- basic.cpp
+++ basic.cpp
@@ -4,18 +4,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        int n=9;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j==4){
-                    break;
-                }
-                else{
-                    cout<<j;
-                }
-            }
-            cout<<endl;
-        }
+
     }
     return 0;
 }
